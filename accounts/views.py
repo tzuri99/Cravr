7a3b3ae0c87585ccc1356902fcc28d99e7dc7eae
HONG ZHI JIE <xavierrr7819@gmail.com>
2026-08-24 00:41:52 +0800

    Still ongoing features 1, will continue tomorrow

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == 'POST':
@@ -11,5 +12,22 @@ def register_view(request):
         form = UserCreationForm()
     
     return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
--- cravr_project/urls.py
+++ cravr_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
 ]
